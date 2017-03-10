7f4495e7a79a93d4ce98cb23ca017b511eb93809
kalaivarathu <kalaikkp776@gmail.com>
2017-03-10 15:47:38 +0530

    first

---
+++ AddressBook/src/mvc/models/Person.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc.models;
+
+/**
+ *
+ * @author Admin
+ */
+public class Person {
+    private String name;
+    private String mob;
+    private String email;
+    private String lastname;
+    private String address;
+    
+   /* public User(){
+        x=0;
+    }
+    
+    public User(int x){
+        this.x=x;
+    }
+    
+    public void incX(){
+        x++;
+    }
+    
+    public void decX(){
+        x--;
+    }
+    public int getX(){
+        return x;
+    }*/
+    public String getName(){
+        return this.name; 
+   }
+     public String getLastname(){
+        return this.lastname;
+    
+   }
+    
+    
+    public String getMob(){
+        return this.mob; 
+   }
+    
+    public String getEmail(){
+        return this.email;
+    }
+       
+         public String getAddress(){
+        return this.address;
+         }
+    public void setData(String name,String lastname, String mob,String email,String address){
+        this.name = name;
+        this.lastname = lastname;
+        this.mob = mob;
+        this.email = email;
+        this.address = address;
+    }
+    
+ 
+
+}
